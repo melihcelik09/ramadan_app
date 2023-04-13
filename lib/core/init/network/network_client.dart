7e4989ac20ad1ecc145450e732b575085d3c69b3
Melih Çelik <melihcelik0909@gmail.com>
2023-04-13 17:38:34 +0300

    add:equatable and endpoints

--- lib/core/init/network/network_client.dart
+++ lib/core/init/network/network_client.dart
@@ -1,9 +1,51 @@
 import 'package:dio/dio.dart';
+import 'package:ramadan_app/core/constants/app_endpoints.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'network_client.g.dart';
 
-@RestApi(baseUrl: "base")
+@RestApi(baseUrl: AppEndpoints.baseUrl)
 abstract class NetworkClient {
   factory NetworkClient(Dio dio, {String baseUrl}) = _NetworkClient;
+
+  @GET("/countries")
+  Future getCountries();
+
+  @GET("/regions")
+  Future getState(@Query("country") String country);
+
+  @GET("/cities")
+  Future getCity(@Query("country") String country, @Query("region") String region);
+
+  @GET("/coordinates")
+  Future getCoordinate(@Query("country") String country, @Query("region") String region, @Query("city") String city);
+
+  @GET("/timesFromCoordinates")
+  Future getTimesFromCoordinates(
+    @Query("lat") String latitude,
+    @Query("lng") String longitude,
+    //2023-10-29
+    @Query("date") String date,
+    @Query("timezoneOffset") String timezone,
+  );
+
+  //baseUrl= https://api.alquran.cloud/v1
+  //Please specify an Ayah number (1 to 6236).
+  @GET("/ayah/{number}/editions/tr.diyanet,en.asad")
+  Future getAyah(@Path("number") String number);
+
+  //baseUrl= https://api.aladhan.com/v1
+  @GET("/gToH/{date}")
+  Future getHijriDate(@Path("date") String date);
+
+  //baseUrl= https://api.aladhan.com/v1
+  @GET("/qibla/{latitude}/{longitude}")
+  Future getQiblaDirection(@Path("latitude") String latitude, @Path("longitude") String longitude);
+
+  @GET("/name")
+  Future getAsmaulHusna(@Header("X-RapidAPI-Key") String key);
+
+  //q= 37.84501,27.83963
+  @GET("/current.json")
+  Future getDailyWeather(@Header("X-RapidAPI-Key") String key, @Query("q") String coordinates);
 }
