dd86595f5639a0e3ad3a9d80ce8329d1058229b6
Melih Çelik <melihcelik0909@gmail.com>
2023-05-31 18:06:18 +0300

    add:appConfiguration

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ramadan_app/app/app.dart';
 import 'package:ramadan_app/core/init/bloc/bloc_list.dart';
+import 'package:ramadan_app/core/init/configuration/configuration.dart';
 
-void main() async {
+void main() async{
+  await appConfiguration();
   runApp(
     MultiBlocProvider(
       providers: [...BlocList.instance.items],
