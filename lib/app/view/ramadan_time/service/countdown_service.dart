eefc044744e0154ee7be10613e3ebe3d677c5e20
SezerKaynak <sezerkaynak00@gmail.com>
2023-06-11 17:19:07 +0300

    update: next ramadan date set automatically

--- lib/app/view/ramadan_time/service/countdown_service.dart
+++ lib/app/view/ramadan_time/service/countdown_service.dart
@@ -1,20 +1,24 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/intl.dart';
 import 'package:ramadan_app/app/view/ramadan_time/model/calendar_model.dart';
 import 'package:ramadan_app/app/view/ramadan_time/model/countdown.dart';
 import 'package:ramadan_app/core/constants/app_endpoints.dart';
 import 'package:ramadan_app/core/init/network/network_client.dart';
 
+int i = 0;
+
 class CountdownService {
   Dio dio = Dio();
+
   Future<Countdown> requestHijriforGregorian() async {
+    String year = "01-09-${1445 + i}";
     final DateFormat formatter = DateFormat("dd-MM-yyyy");
     final DateFormat outputFormatter = DateFormat("yyyy-MM-dd");
 
-    final CalendarModel response = await NetworkClient(dio,
-            baseUrl: AppEndpoints.dateAndQiblaBaseUrl)
-        .requestGregorianforHijri(
-            "77bd26fb72mshd52097b4eaa3e06p1a44b8jsn68fc3dc01507", "01-09-1445");
+    final CalendarModel response =
+        await NetworkClient(dio, baseUrl: AppEndpoints.dateAndQiblaBaseUrl)
+            .requestGregorianforHijri(dotenv.env['X_RAPID_API_KEY']!, year);
 
     final outputDate = formatter.parse(response.data!.gregorian!.date!);
     final String formattedOutput = outputFormatter.format(outputDate);
@@ -23,6 +27,11 @@ class CountdownService {
     Duration timeDifference = dt.difference(DateTime.now());
 
     int days = timeDifference.inDays;
+    if (days < 0) {
+      i += 1;
+      var newCountdown = await requestHijriforGregorian();
+      return newCountdown;
+    }
     int hours = timeDifference.inHours % 24;
     int minutes = timeDifference.inMinutes % 60;
 
